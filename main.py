288f06959f797032e9006e535f4200bc3199e046
Nelson Malbone <nhmalbone@icloud.com>
2024-06-04 18:43:31 -0400

    app 10 udemy course setup so it will only store new events and only new events

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import selectorlib
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
 
+
+# scraping the url provided
 def scrape(url):
     """Scrape the page source from the url"""
     response = requests.get(url)
@@ -11,21 +13,26 @@ def scrape(url):
     return text_source
 
 
+# extracting the text source that we get from the request response
 def extract(text_source):
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
     value = extractor.extract(text_source)["tours"]
     return value
 
 
+# placing extracted data on to a txt file
 def extracted_data(extracted):
     file_path = "extracted_tour_data.txt"
     with open(file_path, 'a') as file:
         file.write(extracted + "\n")
 
 
+# will send a email for new or upcoming events
 def send_email():
     print("Email was sent")
 
+
+# reading the file to beable to tell if a email needs to be sent
 def read(extracted):
     file_path = "extracted_tour_data.txt"
     with open(file_path, 'r') as file:
@@ -34,11 +41,12 @@ def read(extracted):
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
-    extracted_data(extracted)
     content = read(extracted)
     print(extracted)
     # will only send email if a tour is trigger
     if extracted != "No upcoming tours":
         if extracted not in content:
+            # only storing and email data when new events spawn
+            extracted_data(extracted)
             send_email()
 
