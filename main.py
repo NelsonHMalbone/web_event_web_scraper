3de216310c9613b186e6f76743f445630ef82240
Nelson Malbone <nhmalbone@icloud.com>
2024-06-05 18:48:09 -0400

    app 10 udemy course using the from email import emailmessage we succefully got the email to send when a new event was shown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import selectorlib
 import CONFIG
+import smtplib
+from email.message import EmailMessage
 
 
 URL = "https://programmer100.pythonanywhere.com/tours/"
@@ -28,26 +30,43 @@ def extracted_data(extracted):
         file.write(extracted + "\n")
 
 
-# will send a email for new or upcoming events
-def send_email():
-    print("Email was sent")
-
-
-# reading the file to beable to tell if a email needs to be sent
 def read(extracted):
     file_path = "extracted_tour_data.txt"
     with open(file_path, 'r') as file:
         return file.read()
 
+
+# will send a email for new or upcoming events
+def send_email(message):
+    print("send_email function has started")
+    username = CONFIG.key_user_sender
+    password = CONFIG.key_pass
+
+    receiver = CONFIG.key_user_reciever
+    email_message = EmailMessage()
+    email_message["Subject"] = "New Tour Alert!"
+    email_message.set_content("Hey, we just had a new tour event pop up")
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+        server.quit()
+    print("Email was Sent")
+
+# reading the file to beable to tell if a email needs to be sent
+
+
 if __name__ == "__main__":
     scraped = scrape(URL)
     extracted = extract(scraped)
-    content = read(extracted)
     print(extracted)
+    content = read(extracted)
     # will only send email if a tour is trigger
     if extracted != "No upcoming tours":
         if extracted not in content:
             # only storing and email data when new events spawn
             extracted_data(extracted)
-            send_email()
+            send_email(message=f'new concert up in coming {extracted}')
 
